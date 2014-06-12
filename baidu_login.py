0c3891ce4e09be9964ec6e331475f1ac854ab119
lxserenade <lixia625@hotmail.com>
2014-06-13 00:53:53 +0800

    baidu tieba sign in
    only the first one could be signed in;
    under development

--- baidu_login.py
+++ baidu_login.py
@@ -1,3 +1,5 @@
+
+
 import urllib2
 import urllib
 import urllib
@@ -8,12 +10,12 @@ import HTMLParser
 
 
 def tieba():
-    username = 'xxxx'
-    psw = 'xxxx'
+    username = 'XXXXX'
+    psw = 'XXXXXX'
     login_tokenStr = '''bdPass.api.params.login_token='(.*?)';'''
     login_tokenObj = re.compile(login_tokenStr,re.DOTALL)
     #cookiename = 'baidu%s.coockie' % (username)
-    cj = cookielib.LWPCookieJar()
+    cj=cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
     opener.addheaders = [('User-agent','Mozilla/5.0')]
     urllib2.install_opener(opener)
@@ -23,11 +25,10 @@ def tieba():
     r = opener.open(url)
 
     rsp = r.read()
-    
+
     matched_objs = login_tokenObj.findall(rsp)
     if matched_objs:
         token = matched_objs[0]
-        
     post_data = urllib.urlencode({'username':username,
                                     'password':psw,
                                     'token':token,
@@ -46,20 +47,68 @@ def tieba():
     #path = 'http://passport.baidu.com/?login'
     path = 'http://passport.baidu.com/v2/api/?login'
 
-    headers = {'User-Agent' : 'Mozilla 5.10',
-                "Accept-Language": "zh-cn",
-                'charset':'UTF-8'}
+    headers = { 'Accept':'image/webp,*/*;q=0.8',
+                'Accept-Encoding':'gzip,deflate,sdch',
+                'Accept-Language':'en-US,en;q=0.8',
+                'Connection':'keep-alive',
+                'Host':'hm.baidu.com',
+                'Referer':'http://passport.baidu.com/center',
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
+                }
     req = urllib2.Request(path,post_data,headers)
     rsp = urllib2.urlopen(req).read()
     #checkin_pattern = re.compile(r'<a style="float:right;margin-right: 12px;" href="(.*?)" target="_blank">')
     #checkin_result = checkin_pattern.search(rsp)
     itieba='http://tieba.baidu.com/f/like/mylike'
-    rsp = opener.open(itieba).read().decode('gbk')
+    rsp =  urllib2.urlopen(itieba).read().decode('gbk')
     
-    tieba_pattern = re.compile(r'<tr><td><a href="(.*?)title="(.*?)">(.*?)</a></td><td>')
-    tieba_result = checkin_pattern.findall(rsp)
-  
+    checkin_pattern = re.compile(r'<tr><td><a href="(.*?)title="(.*?)">(.*?)</a></td><td>')
+    checkin_result = checkin_pattern.findall(rsp)
 
-    for item in tieba_result:
+
+   
+
+    for item in checkin_result:
         print item[2]
+       
+
+        queryStr1 = 'http://tieba.baidu.com'+item[0]
+        rsp =  urllib2.urlopen(queryStr1).read().decode('gbk')
+       
+        tbs_pattern = re.compile(r'PageData.tbs = "(.*?)";')
+        tbs = tbs_pattern.findall(rsp)
+        t=tbs[0]
+        
+
+
+        post=urllib.urlencode({'ie':'utf-8',
+                                'kw':item[2].encode('utf8'),
+                                'tbs':t.encode('utf8'),
+                                })
+        headers={
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
+                'Accept':'application/json, text/javascript, */*; q=0.01',
+                'Accept-Encoding':'gzip,deflate,sdch',
+                'Accept-Language':'en-US,en;q=0.8',
+                'Connection':'keep-alive',
+                'Content-Length':'88',
+                'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host':'tieba.baidu.com',
+                'Origin':'http://tieba.baidu.com',
+                'Referer':'http://tieba.baidu.com/f?kw=%BB%CA%BC%D2%C2%ED%B5%C2%C0%EF',
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36',
+                'X-Requested-With':'XMLHttpRequest',
+                }
+        path='http://tieba.baidu.com/sign/add'
+        req = urllib2.Request(path,post,headers)
+        rsp = urllib2.urlopen(req).read()
+        print 'DONE'
+
+
+
 tieba()
+
+
+
+
+
